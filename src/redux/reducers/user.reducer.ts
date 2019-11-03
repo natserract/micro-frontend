629e604eae844ec460f54e2cf50c9872336e4e2f
alfinsuryaS <alfins132@gmail.com>
2019-11-03 08:58:34 +0800

    Add redux reducer type

--- src/redux/reducers/user.reducer.ts
+++ src/redux/reducers/user.reducer.ts
@@ -1,13 +1,17 @@
 import { INCREMENT, DECREMENT, RESET } from '../action/action'
 
-const Counter = (state = 0, action: any) => {
+interface State {
+  count: number
+}
+type Action = { type: 'INCREMENT' } | { type: 'DECREMENT' } | { type: 'RESET' }
+
+const Counter = (state: State, action: Action) => {
   switch (action.type) {
-    case INCREMENT:
-      return state + 1
-    case DECREMENT:
-      return state - 1
-    case RESET:
-      return 0
+    case 'INCREMENT':
+      return {
+        ...state,
+        count: state.count + 1
+      }
     default:
       return state
   }
